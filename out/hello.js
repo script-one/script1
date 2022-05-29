0c012bb37028055564b01166a613201a9efb45e1
ccckmit <ccckmit@gmail.com>
2022-05-29 11:52:20 +0800

    merge gen_dart2

--- out/hello.js
+++
@@ -1,6 +0,0 @@
-// source file: prog/hello.s1
-import '../sys/s1.js'
-
-let name='script1';                                // (1) 
-log(('hello '+name));                              // (2) 
-if (typeof main == 'function') main()
--- out/regexp.js
+++
@@ -1,7 +0,0 @@
-// source file: prog/regexp.s1
-import '../sys/s1.js'
-
-let text='name:ccc age:52 height:166 weight:60';   // (1) 
-let r1=re('\\d+','g');                             // (2) 
-log(text.match(r1));                               // (4) 
-if (typeof main == 'function') main()
