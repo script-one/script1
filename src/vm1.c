075d5587fd124cff93f75a8f2091a65770c0a421
ccckmit <ccckmit@gmail.com>
2022-07-16 16:10:30 +0800

    vm1 all op done but not tested!

--- src/vm1.c
+++ src/vm1.c
@@ -42,13 +42,15 @@ int run() {
   printf("==================run================\n");
   env_init();
   ir_t *p = code;
+  obj_t *a = NULL;
+  bool is_stop = false;
   while (1) {
     if (p >= cp) error("no more code...\n");
     ir_t op = *p++;
     char opname[20];
     op_name(op, opname);
     printf("%s ", opname);
-    char *s; int n; double f;
+    char *s; int n; double f; obj_t *o;
     if (op == Float) {
       f = (double) *p++;
       printf(" %lf", f);
@@ -67,27 +69,117 @@ int run() {
       case Fn:
         env_pushf(s); // 新增函數堆疊，將參數加入其中
         break;
+      case Param:
+        break;
       case Ent:
         env_params_end();
         break;
-      case Lev:
+      case Lev: case Ret: 
         env_popf();
         break;
       case Var:
+        a = env_push_var(s);
         break;
       case Get:
+        a = env_get_var(s);
         break;
-      case Src:
+      case Local:
+        a = env_get_local(n);
         break;
-      case Str: 
+      case Str:
+        a = env_get_str(s); 
         break;
       case Float:
+        a = env_get_float(f);
         break;
-      case Narg:
+      case Call:
+        p = *--sp;
         break;
+      case Narg: // do nothing...
+        break;
+      case Jmp:
+        p = p + n;
+        break;
+      case Bz:
+        if (obj_iszero(a)) p = p + n;
+        break;
+      case Bnz:
+        if (!obj_iszero(a)) p = p + n;
+        break;
+      case Store:
+        obj_assign(*--sp, a);
+        break;
+      case Push:
+        *sp++ = a;
+        break;
+      case Print:
+        obj_print(a);
+        break;
+      case Exit:
+        is_stop = true;
+        break;
+      case Lor:
+        a = obj_lor(*--sp, a);
+        break;
+      case Land:
+        a = obj_land(*--sp, a);
+        break;
+      case '!':
+        a = obj_lnot(a);
+        break;
+      case Eq:
+        a = obj_eq(*--sp, a);
+        break;
+      case Neq:
+        a = obj_neq(*--sp, a);
+        break;
+      case Le:
+        a = obj_le(*--sp, a);
+        break;
+      case Ge:
+        a = obj_ge(*--sp, a);
+        break;
+      case Shl:
+        a = obj_shl(*--sp, a);
+        break;
+      case Shr:
+        a = obj_shr(*--sp, a);
+        break;
+      case Neg:
+        a = obj_neg(a);
+        break;
+      case '+':
+        a = obj_add(*--sp, a);
+        break;
+      case '-':
+        a = obj_sub(*--sp, a);
+        break;
+      case '*':
+        a = obj_mul(*--sp, a);
+        break;
+      case '/':
+        a = obj_div(*--sp, a);
+        break;
+      case '%':
+        a = obj_mod(*--sp, a);
+        break;
+      case '&':
+        a = obj_band(*--sp, a);
+        break;
+      case '|':
+        a = obj_bor(*--sp, a);
+        break;
+      case '^':
+        a = obj_bxor(*--sp, a);
+        break;
+      // case Adj:
+      //   break;
+      // case Src:
+      //   break;
       default:
         break;
     }
+    if (is_stop) break;
   }
 }
 
